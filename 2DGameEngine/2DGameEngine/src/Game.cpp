ac58eaec85ffa352864e01ca81eda0ff9bb6be15
JoaoPardal <jflpardal@gmail.com>
2021-03-19 23:15:34 +0000

    player prototype with PNG rendering and position that gets updated over time. Capped FPS

--- 2DGameEngine/2DGameEngine/src/Game.cpp
+++ 2DGameEngine/2DGameEngine/src/Game.cpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 #include <SDL.h>
+#include <SDL_image.h>
+#include <glm/glm.hpp>
 
 Game::Game()
     :m_IsRunning(false)
@@ -11,6 +13,7 @@ Game::Game()
 
 void Game::Initialize()
 {
+    // init SDL
     bool errorInitializingSDL = SDL_Init(SDL_INIT_EVERYTHING) != 0;
     if (errorInitializingSDL)
     {
@@ -18,12 +21,15 @@ void Game::Initialize()
         return;
     }
 
-   m_window = SDL_CreateWindow(
+    // init window
+    // setting the w and h here before using SDL_SetWindowFullscreen()
+    // will set the resolution of the window
+    m_window = SDL_CreateWindow(
         "2D Game Engine", 
         SDL_WINDOWPOS_CENTERED, 
         SDL_WINDOWPOS_CENTERED,
-        800,
-        600,
+        m_windowWidth,
+        m_windowHeight,
         SDL_WINDOW_BORDERLESS
         );
     if(!m_window)
@@ -32,6 +38,7 @@ void Game::Initialize()
         return;
     }
 
+    // init renderer
     m_renderer = SDL_CreateRenderer(m_window, -1, 0);
     if (!m_renderer)
     {
@@ -39,6 +46,7 @@ void Game::Initialize()
         return;
     }
 
+    //SDL_SetWindowFullscreen(m_window, SDL_WINDOW_FULLSCREEN);
     m_IsRunning = true;
 }
 
@@ -62,12 +70,30 @@ void Game::ProcessInput()
     }
 }
 
+glm::vec2 playerPosition;
+glm::vec2 playerVelocity;
+
+void Game::Setup()
+{
+    playerPosition = glm::vec2(10.0, 20.0);
+    playerVelocity = glm::vec2(1.0, 0.0);
+}
+
 void Game::Update()
 {
+    // notice the ; at the end of the line -
+    // this will block processing of the function until the previous frame took millisecs_per_frame
+    while (!SDL_TICKS_PASSED(SDL_GetTicks(), m_millisecondsPreviousFrame + CONST::MILLISECS_PER_FRAME));
+    
+    m_millisecondsPreviousFrame = SDL_GetTicks();
+
+    playerPosition.x += playerVelocity.x;
+    playerPosition.y += playerVelocity.y;
 }
 
 void Game::Run()
 {
+    Setup();
     while (m_IsRunning)
     {
         ProcessInput();
@@ -78,8 +104,22 @@ void Game::Run()
 
 void Game::Render()
 {
-    SDL_SetRenderDrawColor(m_renderer, 0, 0, 0, 255);
+    SDL_SetRenderDrawColor(m_renderer, 21, 21, 21, 255);
     SDL_RenderClear(m_renderer);
+
+    // render tank
+    auto surface = IMG_Load("./assets/images/tank-tiger-right.png");
+    auto texture = SDL_CreateTextureFromSurface(m_renderer, surface);
+    SDL_FreeSurface(surface);
+
+    SDL_Rect dstRect = { 
+        static_cast<int>(playerPosition.x), 
+        static_cast<int>(playerPosition.y),
+        32, 
+        32 
+    };
+    SDL_RenderCopy(m_renderer, texture, NULL, &dstRect);
+    SDL_DestroyTexture(texture);
 
     SDL_RenderPresent(m_renderer);
 }
--- 2DGameEngine/2DGameEngine/src/Game.h
+++ 2DGameEngine/2DGameEngine/src/Game.h
@@ -1,5 +1,12 @@
 #pragma once
 
+// constants
+namespace CONST
+{
+	constexpr int FPS = 60;
+	constexpr int MILLISECS_PER_FRAME = 1000 / CONST::FPS;
+}
+
 class SDL_Window;
 class SDL_Renderer;
 
@@ -12,13 +19,20 @@ public:
 	void Initialize();
 	void Run();
 	void Destroy();
+
+	int m_windowWidth = 800;
+	int m_windowHeight = 600;
 private:
 	void ProcessInput();
 	void Update();
 	void Render();
 
+	void Setup();
+
 	SDL_Window* m_window = nullptr;
 	SDL_Renderer* m_renderer = nullptr;
-	bool m_IsRunning = false;
+
+	bool m_IsRunning = false; 
+	int m_millisecondsPreviousFrame = 0;
 };
 
