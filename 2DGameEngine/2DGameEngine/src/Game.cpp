4bbc7f6e7fe35cba8ac92e1a3554ea38e944724c
JoaoPardal <jflpardal@gmail.com>
2021-03-19 08:45:04 +0000

    Initialize game with the creation of a Window and a Renderer

--- 2DGameEngine/2DGameEngine/src/Game.cpp
+++ 2DGameEngine/2DGameEngine/src/Game.cpp
@@ -1,5 +1,7 @@
 #include "Game.h"
+
 #include <iostream>
+#include <SDL.h>
 
 Game::Game()
 {
@@ -8,6 +10,33 @@ Game::Game()
 
 void Game::Initialize()
 {
+    bool errorInitializingSDL = SDL_Init(SDL_INIT_EVERYTHING) != 0;
+    if (errorInitializingSDL)
+    {
+        std::cerr << "Error initializing SDL" << std::endl;
+        return;
+    }
+
+    SDL_Window* window = SDL_CreateWindow(
+        "2D Game Engine", 
+        SDL_WINDOWPOS_CENTERED, 
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+        SDL_WINDOW_BORDERLESS
+        );
+    if(!window)
+    {
+        std::cerr << "Error creating SDL window" << std::endl;
+        return;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+    if (!renderer)
+    {
+        std::cerr << "Error creating SDL renderer" << std::endl;
+        return;
+    }
 }
 
 void Game::Update()
