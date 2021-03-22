6d59870e165ed0815847035bc561ac6f6f919f1e
JoaoPardal <jflpardal@gmail.com>
2021-03-22 21:28:05 +0000

    Initial implementation of some of the variables and functions of Entity, Component and System

--- 2DGameEngine/2DGameEngine/src/ECS/ECS.cpp
+++ 2DGameEngine/2DGameEngine/src/ECS/ECS.cpp
@@ -1,2 +1,47 @@
 #include "ECS.h"
 
+/// ----------------- ENTITY  ----------------------
+
+Entity::Entity(int id)
+	:m_id(id)
+{
+}
+
+int Entity::GetId() const
+{
+	return m_id;
+}
+
+/// ----------------- COMPONENT ----------------------
+
+unsigned int IComponent::m_nextId = 0;
+
+template<typename T>
+inline unsigned int Component<T>::GetId()
+{
+	const auto id = m_nextId++;
+	return id;
+}
+
+/// ----------------- SYSTEM ----------------------
+void System::AddEntity(Entity entityToAdd)
+{
+
+}
+
+void System::RemoveEntity(Entity entityToRemove)
+{
+
+}
+
+const std::vector<Entity>& System::GetSystemEntities() const
+{
+	return m_entities;
+}
+
+const Signature& System::GetComponentSignature() const
+{
+	return m_componentSignature;
+}
+
+/// ----------------- REGISTRY ----------------------
--- 2DGameEngine/2DGameEngine/src/ECS/ECS.h
+++ 2DGameEngine/2DGameEngine/src/ECS/ECS.h
@@ -1,21 +1,72 @@
 #pragma once
 
+#include <bitset>
+#include <vector>
+
+namespace CONST
+{
+	namespace ECS
+	{
+		constexpr unsigned int MAX_COMPONENTS = 32;
+	}
+}
+
+
+// Signature represents the components that a Entity has or that
+// an entity must have for a system to work on it
+typedef std::bitset<CONST::ECS::MAX_COMPONENTS> Signature;
+
 class Entity
 {
+public:
+	Entity(int id);
 
+	int GetId() const;
+
+private:
+	int m_id;
 };
 
-class Component
+struct IComponent
 {
+protected:
+	static unsigned int m_nextId;
+};
 
+// used o assign a unique id to a component type
+template <typename T>
+class Component : public IComponent
+{
+public:
+	static unsigned int GetId();
 };
 
 class System
 {
+public:
+	System() = default;
+	~System() = default;
 
+	void AddEntity(Entity entityToAdd);
+	void RemoveEntity(Entity entityToRemove);
+	const std::vector<Entity>& GetSystemEntities() const;
+	const Signature& GetComponentSignature() const;
+
+	// what components will be a part of this system's signature?
+	template <typename TComponent> void RequireComponent();
+private:
+	Signature m_componentSignature;
+	std::vector<Entity> m_entities;
 };
 
 class Registry
 {
 
 };
+
+template <typename TComponent>
+void System::RequireComponent()
+{
+	const auto componentId = Component<TComponent>::GetId();
+	m_componentSignature.set(componentId);
+}
