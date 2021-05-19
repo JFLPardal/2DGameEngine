33b680c1636fb15a7fb88dbe18f7fbb8ecdad10b
JoaoPardal <jflpardal@gmail.com>
2021-05-19 21:28:14 +0100

    Updated the player controlled character movement * Character no longer moves by just pressing a key, the key needs to be held down; * Character has momentum, it does not stop as soon as the movement key is released; * Character can now move diagonally.

--- 2DGameEngine/2DGameEngine/src/Systems/KeyboardControlSystem.h
+++ 2DGameEngine/2DGameEngine/src/Systems/KeyboardControlSystem.h
@@ -28,9 +28,10 @@ public:
 		RequireComponent<RigidbodyComponent>();
 		RequireComponent<SpriteComponent>();
 	}
+
 	void SubscribeToEvents(std::unique_ptr<EventBus>& eventBus)
 	{
-		eventBus->SubscribeToEvent<KeyPressedEvent>(this, &KeyboardControlSystem::OnKeyPressed);
+		//eventBus->SubscribeToEvent<KeyPressedEvent>(this, &KeyboardControlSystem::OnKeyPressed);
 	}
 
 	void OnKeyPressed(KeyPressedEvent& keyPressedEvent)
@@ -46,7 +47,7 @@ public:
 				rigidbody.m_velocity = velocity;
 				sprite.m_textureRect.y = sprite.m_height * static_cast<int>(ySpriteIndex);
 			};
-
+			
 			switch (keyPressedEvent.m_keyPressed)
 			{
 			case(SDL_KeyCode::SDLK_w):
@@ -62,7 +63,75 @@ public:
 				SetVelocityAndTextureRect(keyboardControl.m_leftVelocity, Direction::left);
 				break;
 			}
+		}
+	}
 
+	void Update()
+	{
+		for (auto entity : GetSystemEntities())
+		{
+			const auto& keyboardControl = entity.GetComponent<KeyboardControlledComponent>();
+			auto& rigidbody = entity.GetComponent<RigidbodyComponent>();
+
+			auto keyboardStateArray = SDL_GetKeyboardState(NULL);
+
+			bool isMovingInX = false;
+			bool isMovingInY = false;
+			glm::vec2 newVelocity = rigidbody.m_velocity;//{ 0, 0 };
+			if (keyboardStateArray[SDL_SCANCODE_W])
+			{
+				newVelocity.y = keyboardControl.m_upVelocity.y;
+				isMovingInY = true;
+			}
+			if (keyboardStateArray[SDL_SCANCODE_A])
+			{
+				newVelocity.x = keyboardControl.m_leftVelocity.x;
+				isMovingInX = true;
+			}
+			if (keyboardStateArray[SDL_SCANCODE_S])
+			{
+				newVelocity.y = keyboardControl.m_downVelocity.y;
+				isMovingInY = true;
+			}
+			if (keyboardStateArray[SDL_SCANCODE_D])
+			{
+				newVelocity.x = keyboardControl.m_rightVelocity.x;
+				isMovingInX = true;
+			}
+			//TODO normalize velocity for diagonal speed
+			// TODO cleanup/simplify the code, this is messy as hell
+			if (!isMovingInX)
+			{
+				const bool isMovingRight = rigidbody.m_velocity.x > 0;
+				const auto velocityDecrementInX = 
+					(isMovingRight) ?
+					static_cast<int>(rigidbody.m_velocity.x - keyboardControl.m_rightVelocity.x * .01f) :
+					static_cast<int>(rigidbody.m_velocity.x + keyboardControl.m_rightVelocity.x * .01f);
+
+				const int maxVelocity = keyboardControl.m_rightVelocity.x;
+				newVelocity.x = 
+					(isMovingRight) ?
+					glm::clamp(velocityDecrementInX, 0, maxVelocity) :
+					glm::clamp(velocityDecrementInX, -maxVelocity, 0);
+			}
+			if (!isMovingInY)
+			{
+				const bool isMovingUp = rigidbody.m_velocity.y < 0;
+				const auto velocityDecrementInY = 
+					(isMovingUp) ?
+					static_cast<int>(rigidbody.m_velocity.y - keyboardControl.m_upVelocity.y * .01f) :
+					static_cast<int>(rigidbody.m_velocity.y + keyboardControl.m_upVelocity.y * .01f);
+
+				const int maxVelocity = keyboardControl.m_upVelocity.y;
+				newVelocity.y = 
+					(isMovingUp) ?
+					glm::clamp(velocityDecrementInY, maxVelocity, 0) :
+					glm::clamp(velocityDecrementInY, 0, -maxVelocity)
+					;
+
+				Logger::Error(std::to_string(newVelocity.y));
+			}
+			rigidbody.m_velocity = newVelocity;
 		}
 	}
 };
